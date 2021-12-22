47a16365fe0b7cfb5ca7e47d65c498fef5833509
error510 <96317406+error510@users.noreply.github.com>
2021-12-22 11:51:11 +0100

    Update ex4.py

--- unit 1/ex/ex4.py
+++ unit 1/ex/ex4.py
@@ -1,13 +1,28 @@
+# to make sure both exceptions are excuted i added another variable 
 try:
-  a = 12
+  a =  12.5
+  b = 12
   s = "hello"
-  print(a+s)
-except TypeError : 
-    print(a,s) 
-except TypeError : 
- if (type(a) == int):
-         a = str(a)
+  print(a+s+b)
 
+except TypeError : 
+    if (type(b) == int or type(a) == int):
+        a = str(a)
+        b = str(b)
+        print(a+s+b)
+    else :
+      print(a,s,b)
+# or another shorter way
+try:
+  o = 12
+  i = "hello"
+  print(i+o)
+
+except TypeError : 
+    if (type(o) != str ):
+        o = str(o)
+        print(i,o) # print(i+o)
+ 
 
 
 
